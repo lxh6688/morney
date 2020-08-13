830010bcc59a65ee232c317033265a0dfd92d7b7
codetiger <1272662790@qq.com>
2020-08-14 07:23:40 +0800

    添加LayOut

--- src/main.ts
+++ src/main.ts
@@ -4,10 +4,12 @@ import './registerServiceWorker'
 import router from './router'
 import store from './store'
 import Nav from '@/components/Nav.vue'
+import LayOut from '@/components/LayOut.vue'
 
 Vue.config.productionTip = false
 
 Vue.component('Nav',Nav)
+Vue.component('LayOut',LayOut)
 
 new Vue({
   router,
