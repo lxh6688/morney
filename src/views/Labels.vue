269da3fc45ecfd9d4f27780996131d2500e17846
codetiger <1272662790@qq.com>
2020-08-14 07:05:00 +0800

    重复三次布局

--- src/views/Labels.vue
+++ src/views/Labels.vue
@@ -1,6 +1,8 @@
 <template>
-    <div>
-        Labels
+   <div class="nav-wrapper">
+        <div class="content">        
+            Labels.vue
+        </div>
         <Nav/>
     </div>
 </template>
@@ -12,5 +14,14 @@
 </script>
 
 <style lang="scss" scoped>
-
+    .nav-wrapper{
+    border:1px solid green;
+    display: flex;
+    flex-direction: column;
+    height:100vh;
+}
+.content{
+    overflow: auto;
+    flex-grow:1;
+}
 </style>
