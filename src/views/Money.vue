a37b59264f95280adcace178d51d382c5f9bd389
codetiger <1272662790@qq.com>
2020-08-22 19:18:42 +0800

    添加createdAt字段

--- src/views/Money.vue
+++ src/views/Money.vue
@@ -1,6 +1,6 @@
 <template>
   <Layout class-prefix="layout">
-    {{record}}
+    {{recordList}}
     <NumberPad @update:value="onUpdateAmount" @submit="saveRecord"/>
     <Types :value.sync="record.type"/>
     <Notes @update:value="onUpdateNotes"/>
@@ -16,11 +16,13 @@
   import Tags from '@/components/Money/Tags.vue';
   import {Component,Watch} from 'vue-property-decorator';
   
+
   type Record = {
     tags: string[];
     notes: string;
     type: string;
     amount: number;
+    createdAt?: Date;
   }
 
   @Component({
@@ -28,7 +30,7 @@
   })
   export default class Money extends Vue {
     tags = ['衣','食','住','行'];
-    recordList: Record[] = [];
+    recordList: Record[] = JSON.parse(window.localStorage.getItem('recordList') || '[]');
     record: Record = {
       tags: [], notes: '', type: '-', amount: 0
     };
@@ -43,7 +45,8 @@
       this.record.amount = parseFloat(value);
     }
     saveRecord(){
-      const record2 = JSON.parse(JSON.stringify(this.record))
+      const record2: Record = JSON.parse(JSON.stringify(this.record));
+      record2.createdAt = new Date();
       this.recordList.push(record2);
       console.log(this.recordList)
     }
