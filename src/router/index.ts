8635577800bc4f028ea45c978e38b859161a4e25
codetiger <1272662790@qq.com>
2020-08-26 22:07:34 +0800

    将 tag 的 string 改为 {id, name}

--- src/router/index.ts
+++ src/router/index.ts
@@ -5,6 +5,7 @@ import Money from '@/views/Money.vue';
 import Labels from '@/views/Labels.vue';
 import Statistics from '@/views/Statistics.vue';
 import NotFound from '@/views/NotFound.vue';
+import EditLabel from '@/views/EditLabel.vue'
 
 
 Vue.use(VueRouter);
@@ -27,22 +28,13 @@ Vue.use(VueRouter);
       component:Statistics
     },
     {
+      path:'/labels/edit',
+      component:EditLabel
+    },
+    {
       path: '*',
       component:NotFound
     }
-  // {
-  //   path: '/',
-  //   name: 'Home',
-  //   component: Home
-  // },
-  // {
-  //   path: '/about',
-  //   name: 'About',
-  //   // route level code-splitting
-  //   // this generates a separate chunk (about.[hash].js) for this route
-  //   // which is lazy-loaded when the route is visited.
-  //   component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  // }
 ]
 
 const router = new VueRouter({
---
+++ src/views/EditLabel.vue
@@ -0,0 +1,19 @@
+<template>
+    <Layout>
+        编辑标签
+    </Layout>
+</template>
+
+<script>
+    import Vue from 'vue';
+    import { Component } from 'vue-property-decorator';
+
+    @Component
+    export default class EditLabel extends Vue{
+        
+    }
+</script>
+
+<style lang="sass" scoped>
+
+</style>
