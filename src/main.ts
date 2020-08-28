c8930db6e307f1424af7ab4778f0c2a6493f406d
codetiger <1272662790@qq.com>
2020-08-28 22:41:47 +0800

    使用全局管理 tagList

--- src/main.ts
+++ src/main.ts
@@ -1,17 +1,20 @@
-import Vue from 'vue'
-import App from './App.vue'
-import './registerServiceWorker'
-import router from './router'
-import store from './store'
-import Nav from '@/components/Nav.vue'
-import Layout from '@/components/LayOut.vue'
-import Icon from '@/components/Icon.vue'
+import Vue from 'vue';
+import App from './App.vue';
+import './registerServiceWorker';
+import router from './router';
+import store from './store';
+import Nav from '@/components/Nav.vue';
+import Layout from '@/components/LayOut.vue';
+import Icon from '@/components/Icon.vue';
+import tagListModel from '@/models/tagListModel';
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
 
-Vue.component('Nav',Nav)
-Vue.component('Layout',Layout)
-Vue.component('Icon',Icon)
+Vue.component('Nav',Nav);
+Vue.component('Layout',Layout);
+Vue.component('Icon',Icon);
+
+window.tagList = tagListModel.fetch();
 
 new Vue({
   router,
