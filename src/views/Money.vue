2ed34e9e6aed9ee47faaec514da02cd23d8a207b
codetiger <1272662790@qq.com>
2020-08-28 22:28:59 +0800

    将 create 封装到recordListModel

--- src/views/Money.vue
+++ src/views/Money.vue
@@ -46,15 +46,12 @@
       this.record.amount = parseFloat(value);
     }
     saveRecord(){
-      const record2: RecordItem = recordListModel.clone(this.record);
-      record2.createdAt = new Date();
-      this.recordList.push(record2);
-      console.log(this.recordList)
+      recordListModel.create(this.record)
     }
 
     @Watch('recordList')
     onRecordListChange(){
-      recordListModel.save(this.recordList);
+      recordListModel.save(recordList);
     }
   }
 </script>
