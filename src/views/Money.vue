bb39e700dd71b3a167675ced0627fe901d15e509
codetiger <1272662790@qq.com>
2020-08-30 15:05:11 +0800

    将多有数据操作封装到 store 里

--- src/views/Money.vue
+++ src/views/Money.vue
@@ -18,17 +18,15 @@
   import Types from '@/components/Money/Types.vue';
   import FormItem from '@/components/Money/FormItem.vue';
   import Tags from '@/components/Money/Tags.vue';
-  import {Component,Watch} from 'vue-property-decorator';
-  import recordListModel from '@/models/recordListModel';
-  
-  const recordList = recordListModel.fetch();
+  import {Component} from 'vue-property-decorator';
+  import store from '@/store/index2.ts';
 
   @Component({
      components: {Tags, FormItem, Types, NumberPad}
   })
   export default class Money extends Vue {
-    tags = window.tagList;
-    recordList: RecordItem[] = recordList;
+    tags = store.tagList;
+    recordList= store.recordList;
     record: RecordItem = {
       tags: [], notes: '', type: '-', amount: 0
     };
@@ -43,12 +41,7 @@
       this.record.amount = parseFloat(value);
     }
     saveRecord(){
-      recordListModel.create(this.record)
-    }
-
-    @Watch('recordList')
-    onRecordListChange(){
-      recordListModel.save(recordList);
+      store.createRecord(this.record)
     }
   }
 </script>
